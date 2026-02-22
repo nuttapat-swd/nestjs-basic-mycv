d31e3caa797d9c5581d0fab45990b746d04c6cf3
Tum MacM4 <nuttapat.swd@gmail.com>
2026-02-23 00:47:32 +0700

    feat: implement authentication service with signup and signin functionality, add guards and interceptors for user management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,33 +8,56 @@ import {
     Param,
     Query,
     NotFoundException,
-    UseInterceptors,
-    ClassSerializerInterceptor
+    Session,
+    UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './users.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) {} 
+
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user: User) { return user; }
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.usersService.create(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signout(@Session() session: any) {
+        session.userId = null;
     }
 
     @Get('/:id')
     async findUser(@Param('id') id: string) {
-        console.log("start handling")
         const user = await this.usersService.findOne(parseInt(id));
         if (!user) {
             throw new NotFoundException('User not found');
         }
-        console.log("end handling")
         return user;
     }
 
